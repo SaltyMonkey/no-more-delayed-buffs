752a904600dc551e56e7eb5f7fb03ae268096e73
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-08-16 09:59:54 +0300

    typos

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = function noMoreDelayedBuffs(mod) {
 	if (mod.majorPatchVersion < 97) {
-		mod.log("This mod fixing insues with x64 client only. Unavailable with your patch");
+		mod.log("This mod fixing issues with x64 client only. Unavailable with your patch");
 		return;
 	}
 
 	mod.game.initialize("me");
 	
 	mod.hook("S_ABNORMALITY_REFRESH", 2, { "order": 99999999, "filter": { "fake": null } }, (event) => {
+		if (event.target !== mod.game.me.gameId || event.stacks <= 1) return;
 
-		if (event.target !== mod.game.me.gameId) return;
 		mod.send("S_ABNORMALITY_END", 1, {
 			"target": mod.game.me.gameId,
 			"id": event.id
